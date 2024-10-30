4da0da5dec25f62e4cfe575b1538d3cc5890e938
mondk <66952082+mondk@users.noreply.github.com>
2024-10-30 13:04:28 +0100

    op

--- src/PrinterInterface.java
+++ src/PrinterInterface.java
@@ -4,13 +4,13 @@ import java.util.Queue;
 
 public Interface Printer extends Remote {
 
-    public void print(String filename) throws RemoteException;
-    public Queue<String> queue() throws RemoteException;
-    public void TopQueue(int job) throws RemoteException;
+    public void print(String filename, String printer) throws RemoteException;
+    public Queue<String> queue(String printer) throws RemoteException;
+    public void TopQueue(String, printer, int job) throws RemoteException;
     public void start() throws RemoteException;
     public void stop() throws RemoteException;
     public void restart() throws RemoteException;
-    public String status() throws RemoteException;
+    public String status(String printer) throws RemoteException;
     public String readConfig(String parameter) throws RemoteException;
     public void setConfig(String parameter, String value) throws RemoteException;
 
