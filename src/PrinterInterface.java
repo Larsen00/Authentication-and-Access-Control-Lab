a3b81d8e1a0f0456290d9512b954092d0c84e2a5
Larsen00 <jonas.larsen103@gmail.com>
2024-11-17 13:11:29 +0100

    public password

--- src/PrinterInterface.java
+++ src/PrinterInterface.java
@@ -1,3 +1,4 @@
+import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.Queue;
@@ -14,6 +15,9 @@ public interface PrinterInterface extends Remote {
     public void setConfig(String parameter, String value, SessionToken sessionToken) throws RemoteException, PrintAppException;
     public String displayPrinters(SessionToken sessionToken) throws  RemoteException, PrintAppException;
     public void accessControl(String methodName, String[] userRole) throws Exception;
-    public SessionToken login(String username, String password) throws PrintAppException, RemoteException;
+    public Response<PrinterInterface> login(String username, String password) throws RemoteException, PrintAppException;
     SessionToken authenticateUser(String username, String password, SessionToken sessionToken, String action) throws PrintAppException, RemoteException;
+
+    public boolean isAllowedToStartServer(String username) throws RemoteException;
+   public Response<PrinterInterface> connect(String username, String password, SessionToken sessionToken) throws RemoteException, PrintAppException;
 }
